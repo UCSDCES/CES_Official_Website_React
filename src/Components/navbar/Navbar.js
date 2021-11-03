290a6073073a7b9c6429ca34a16a9696a0fa5e2f
Cryszzz <crystalzou000829@gmail.com>
2021-11-02 22:22:22 -0700

    update-responsive

--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -10,20 +10,21 @@ const navbar = () => (
 
 
         <Navbar.Brand id="nav-brand" href="/">CHINESE ENGINEERING SOCIETY</Navbar.Brand>
+        <Navbar.Brand id="nav-brand-small" href="/">C E S</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" style={{float: "right"}}>
             <Nav >
-                <Nav.Item style={{float: "right"}}><Nav.Link id="nav-item" className="active" href="/">Home</Nav.Link></Nav.Item>
-                <Nav.Item style={{float: "right"}}><Nav.Link id="nav-item" href="/members">Members</Nav.Link></Nav.Item>
-                <Nav.Item style={{float: "right"}}><Nav.Link id="nav-item" href="/gallery">Gallery</Nav.Link></Nav.Item>
-                <Nav.Item style={{float: "right"}}><Nav.Link id="nav-item" href="/events">Events</Nav.Link></Nav.Item>
-                <Nav.Item style={{float: "right"}}><Nav.Link id="nav-item" href="/about">About</Nav.Link></Nav.Item>
-                <Nav.Item style={{float: "right"}}><Nav.Link id="nav-item" href="#link">Contact</Nav.Link></Nav.Item>
+                <Nav.Item style={{float: "right"}}><Nav.Link activeclassName="active" href="/">Home</Nav.Link></Nav.Item>
+                <Nav.Item style={{float: "right"}}><Nav.Link activeClassName="active" href="/members">Members</Nav.Link></Nav.Item>
+                <Nav.Item style={{float: "right"}}><Nav.Link activeClassName="active" href="/gallery">Gallery</Nav.Link></Nav.Item>
+                <Nav.Item style={{float: "right"}}><Nav.Link activeClassName="active" href="/events">Events</Nav.Link></Nav.Item>
+                <Nav.Item style={{float: "right"}}><Nav.Link activeClassName="active" href="/about">About</Nav.Link></Nav.Item>
+                <Nav.Item style={{float: "right"}}><Nav.Link activeClassName="active" href="#link">Contact</Nav.Link></Nav.Item>
             </Nav>
         </Navbar.Collapse>
         </Container>
     </Navbar>
 
 )
-
 export default navbar;
+
