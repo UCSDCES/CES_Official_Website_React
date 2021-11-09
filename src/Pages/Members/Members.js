2b6d5eb21e74abdac67106e3801c55b6e799f719
jaaack233 <jackchen990227@gmail.com>
2021-11-08 21:55:20 -0800

    update member list

--- src/Pages/Members/Members.js
+++ src/Pages/Members/Members.js
@@ -27,64 +27,81 @@ const Members = () => (
             <section>
                 <h2>Leaderboard</h2>
                 <Row>
-                    <MemberCard name='Wanjun Gu' info="President" role="President" />
-                    <MemberCard name='Weiwen Hong' info="Senior | Computer Science" role="Vice President" />
-                    <MemberCard name='Xingqi Wang' info="Senior | Biochem & Cell Biology" role="Lead" department="CDP"/>
-                    <MemberCard name='Duolan Ouyang' info="Junior | Computer Science" role="Lead" department="CDP"/>
-                    <MemberCard name='Yueqing Qiao' info="Junior | Bioengineering: Bioinformatics" role="Lead" department="Media, DAD"/>
-                    <MemberCard name='Huiqi Chen' info="Freshmen | Economics & Communication" role="Lead" department='BD' />
-                    <MemberCard name='Zhuoran Shao' info="Sophomore | Cognitive Science, Machine Learning" role="Lead" department='BD' />
-                    <MemberCard name='Hairui Hou' info="Junior | Neurobiology" role="Lead" department='DAD' />
-                    <MemberCard name='Yuelai Wang' info="Junior | Bioengineering: Bioinformatics" role="Lead" department='DAD' />
+                    <MemberCard name='Yueqing Qiao' info="Senior | Bioengineering: Bioinformatics" role="President" />
+                    <MemberCard name='Duolan Ouyang' info="Senior | Computer Science" role="Vice President" />
+                    <MemberCard name='Yuelai Wang' info="Senior | Bioengineering: Bioinformatics" role="Vice President" />
+                    <MemberCard name='Yuqiu Wang' info="Senior | Bioengineering: Bioinformatics" role="Lead" department="Media"/>
+                    <MemberCard name='Chi Zhang' info="Junior | Bioengineering: Bioinformatics" role="Lead" department="Media"/>
+                    <MemberCard name='Fangyi Lu' info="Senior | Bioengineering: Biosystem" role="Lead" department="CDP"/>
+                    <MemberCard name='Manshi Yang' info="Junior | Prob & Stats, CE" role="Lead" department="CDP"/>
+                    <MemberCard name='Huiqi Chen' info="Sophomore | Economics & Communication" role="Lead" department='BD' />
+                    <MemberCard name='Zhuoran Shao' info="Junior | Cognitive Science, Machine Learning" role="Lead" department='BD' />
+                    <MemberCard name='Yuntian Bi' info="Senior | Gen Chem & ESYS " role="Lead" department='DAD' />
+                    <MemberCard name='Tianze Guo' info="Senior | Bioengineering: Biotechnology" role="Lead" department='DAD' />
+                    <MemberCard name='Yicheng Guo' info="Senior | Math-CS" role="Lead" department='HR' />
+                    <MemberCard name='Lehan Li' info="Senior | Data Science & Cognitive Science" role="Lead" department='HR' />
                 </Row>
             </section>
             <section>
                 <h2>Members</h2>
                 <Row>
-                    <MemberCard name='Yilang He' info="Sophomore | Math-CS" role="Member" department="CDP" />
-                    <MemberCard name='Manshi Yang' info="Sophomore | Probability & Statistics" role="Member" department="CDP" />
-                    <MemberCard name='Jiawen Yu' info="Sophomore | Electrical Engineering" role="Member" department="CDP" />
-                    <MemberCard name='Fangyi Lu' info="Sophomore | Bioengineering" role="Member" department="CDP" />
-                    <MemberCard name='Yiqiao Hao' info="Freshmen | Biochem" role="Member" department="CDP" />
-                    <MemberCard name='Yuexiang Zhang' info="Senior | Data Science" role="Member" department="CDP" />
-                    <MemberCard name='Bihua Zhao' info="Freshmen | Maths-Computer Science & Management Science" role="Member" department="CDP" />
-                    <MemberCard name='Haihao Sun' info="Junior | Computer Engineering" role="Member" department="CDP" />
-                    <MemberCard name='Mingyu Fangao' info="Sophomore | Applied Math" role="Member" department="CDP" />
-                    <MemberCard name='Yuru Zhou' info="Sophomore | Mathematics & Computer Science" role="Member" department="CDP" />
+                    <MemberCard name='Shijie Huang' info="Master | CSE" role="Member" department="CDP" />
+                    <MemberCard name='Yilang He' info="Junior | Math-CS" role="Member" department="CDP" />
+                    <MemberCard name='Jiawen Yu' info="Junior | Electrical Engineering" role="Member" department="CDP" />
+                    <MemberCard name='Yiqiao Hao' info="Sophomore | Biochem" role="Member" department="CDP" />
+                    <MemberCard name='Bihua Zhao' info="Sophomore | Maths-Computer Science & Management Science" role="Member" department="CDP" />
+                    <MemberCard name='Mingyu Fangao' info="Junior | Applied Math" role="Member" department="CDP" />
+                    <MemberCard name='Ruinan Ma' info="Freshmen | Undeclared" role="Member" department="CDP" />
+                    <MemberCard name='Yucheng Huang' info="Junior | CE" role="Member" department="CDP" />
+                    <MemberCard name='Chen Lou' info="Junior | CE" role="Member" department="CDP" />
+                    <MemberCard name='Zhaoyu Zhang' info="Sophomore | Math-CS" role="Member" department="CDP" />
+                    <MemberCard name='Zishan Gao' info="Junior | CE" role="Member" department="CDP" />
+                    <MemberCard name='Mingxuan Nie' info="Freshmen | EE" role="Member" department="CDP" />
+                    <MemberCard name='Bihua Zhao' info="Sophomore | Math-CS" role="Member" department="CDP" />
 
-                    <MemberCard name='Jiadong Chen' info="Junior | Cognitive Science, HCI" role="Member" department="Media" />
-                    <MemberCard name='Yueqian Ma' info="Junior | Computer Science" role="Member" department="Media, BD" />
-                    <MemberCard name='Chi Zhang' info="Sophomore | Bioengineering: Biotech" role="Member" department="Media" />
-                    <MemberCard name='Rongyu Ma' info="Junior | CSE: Bioinformatics" role="Member" department="Media" />
+                    <MemberCard name='Jiadong Chen' info="Senior | Cognitive Science: HCI" role="Member" department="Media" />
+                    <MemberCard name='Rongyu Ma' info="Senior | CSE: Bioinformatics" role="Member" department="Media" />
+                    <MemberCard name='Yupeng Sun' info="Sophomore | Math-CS" role="Member" department="Media" />
+                    <MemberCard name='Jian Qiu' info="Junior | Math&Econ" role="Member" department="Media" />
+                    <MemberCard name='Zhaoyue Xu' info="Senior | Business Psych" role="Member" department="Media" />
+                    <MemberCard name='Dan Yin' info="Senior | Biology Bioinformatics" role="Member" department="Media" />
+                    <MemberCard name='Xianglin Chen' info="Sophomore | EE" role="Member" department="Media" />
+                    <MemberCard name='Xiaoxiao Zou' info="Junior | CE, Applied Math" role="Member" department="Media" />
+                    <MemberCard name='Xuanyu Dong' info="Junior | Neurobiology" role="Member" department="Media, BD" />
+                    <MemberCard name='Yuxin Wang' info="Senior | Econ" role="Member" department="Media" />
+
 
                     <MemberCard name='Dongyang Li' info="Freshmen | Mathematics & Computer Science" role="Member" department="BD" />
                     <MemberCard name='Xuanyu Dong' info="Sophomore | Neurobiology" role="Member" department="BD" />
 
-                    <MemberCard name='Xinyi Zhu' info="Junior | Biology: Bioinformatics" role="Member" department="DAD" />
-                    <MemberCard name='Shunkai Yu' info="Junior | Computer Engineering" role="Member" department="DAD" />
-                    <MemberCard name='Yunrian Bi' info="Junior | General Chemistry" role="Member" department="DAD" />
-                    <MemberCard name='Siyin Yang' info="Sophomore | Biotechnology" role="Member" department="DAD" />
-                    <MemberCard name='Tianze Guo' info="Junior | Biotechnology" role="Member" department="DAD" />
-                    <MemberCard name='Ziyi Wang' info="Junior | Biotechnology" role="Member" department="DAD" />
-                    <MemberCard name='Junyi Xu' info="Freshmen | Electrical Engineering" role="Member" department="DAD" />
-                    <MemberCard name='Yuli Wu' info="Freshmen | Chemistry / Biochemistry" role="Member" department="DAD" />
-                    <MemberCard name='Qiuyu Wang' info="Junior | BioEngineering: Bioinformatics" role="Member" department="DAD" />
-                    <MemberCard name='Bangjun Li' info="Freshmen | Math-P&S" role="Member" department="DAD" />
-                    <MemberCard name='Yi Mao' info="Junior | Mechanical Engineering" role="Member" department="DAD" />
-                    <MemberCard name='Xun Liu' info="Freshmen | Chemistry" role="Member" department="DAD" />
+                    <MemberCard name='Xinyi Zhu' info="Senior | Biology: Bioinformatics" role="Member" department="DAD" />
+                    <MemberCard name='Shunkai Yu' info="Senior | Computer Engineering" role="Member" department="DAD" />
+                    <MemberCard name='Ziyi Wang' info="Senior | Biotechnology" role="Member" department="DAD" />
+                    <MemberCard name='Junyi Xu' info="Senior | Electrical Engineering" role="Member" department="DAD" />
+                    <MemberCard name='Yuli Wu' info="Sophomore | Chemistry / Biochemistry" role="Member" department="DAD" />
+                    <MemberCard name='Bangjun Li' info="Sophomore | Math-P&S" role="Member" department="DAD" />
+                    <MemberCard name='Yi Mao' info="Senior | Mechanical Engineering" role="Member" department="DAD" />
+                    <MemberCard name='Xun Liu' info="Sophomore | Chemistry" role="Member" department="DAD" />
+                    <MemberCard name='Yang Liu' info="Sophomore | Biochem" role="Member" department="DAD, HR" />
+                    <MemberCard name='Jiachen Xi' info="Sophomore | Bioengineering: Bioinformatics" role="Member" department="DAD" />
+                    <MemberCard name='Tianyi Li' info="Freshmen | Math" role="Member" department="DAD" />
+                    <MemberCard name='Haoran Meng' info="Sophomore | Bioengineering:Bioengineering" role="Member" department="DAD" />
 
-                    <MemberCard name='Yicheng Guo' info="Senior | Math-CS" role="Member" department="IA" />
-                    <MemberCard name='Yu Cao' info="Junior | Data Science" role="Member" department="IA" />
-                    <MemberCard name='Yiwei Cao' info="Senior | Math-Econ" role="Member" department="IA" />
-                    <MemberCard name='Lehan Li' info="Junior | Data Science / Cognitive Science" role="Member" department="IA" />
-                    <MemberCard name='Changhong Chen' info="" role="Member" department="IA" />
+                    <MemberCard name='Zixin Ma' info="Junior | Data Science" role="Member" department="HR" />
+                    <MemberCard name='Yu Cao' info="Junior | Data Science" role="Member" department="HR" />
+                    <MemberCard name='Yiwei Cao' info="Senior | Math-Econ" role="Member" department="HR" />
+                    <MemberCard name='Changhong Chen' info="Senior | Mechanical Engineering" role="Member" department="HR" />
+                    <MemberCard name='Hongyue Jin' info="Junior | COGS, CE" role="Member" department="HR" />
+                    <MemberCard name='Haofeng Chen' info="Junior | Mechanical Engineering" role="Member" department="HR" />
                 </Row>
 
             </section>
             <section>
                 <h2>Alumni</h2>
                 <Row>
+                    <AlumniCard name='Wanjun Gu' role='Former President'/>
                     <AlumniCard name='Zhizhen Qin' role='Former President'/>
+                    <AlumniCard name='Weiwen Hong' role='Former VP'/>
                     <AlumniCard name='Xinyu Liu' role='Former Strategy'/>
                     <AlumniCard name='Zhiyuan Chen' role=''/>
                     <AlumniCard name='Shuyang Liu' role='Former DAD'/>
@@ -120,6 +137,7 @@ const Members = () => (
                     <AlumniCard name='Xiaoye Zuo' role=''/>
                     <AlumniCard name='Shinuo Xu' role='Former CDP'/>
                     <AlumniCard name='Yiran Shao' role='Former CDP'/>
+                    <AlumniCard name='Yuexiang Zhang' role="Former CDP" />
 
                 </Row>
 
