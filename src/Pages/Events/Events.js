da04567b847671a856d2433160e8a4afe00b5393
jaaack233 <jackchen990227@gmail.com>
2022-02-21 21:28:56 -0800

    delete future events

--- src/Pages/Events/Events.js
+++ src/Pages/Events/Events.js
@@ -19,8 +19,7 @@ const Events = () => (
         <h1 style={{ marginTop: "20px", marginBottom: "20px", color: "#b54648" }}>Upcoming</h1>
         <div className='event-container' >
             <Row>
-                <EventCard date='Nov. 11th' title='Research Seminar' image={pic} intro='' />
-                <EventCard date='Week 10' title='Round table' image={pic} intro='' />
+            
             </Row>
 
         </div>
